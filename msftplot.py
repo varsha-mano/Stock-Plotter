861c13c61e0f29bffe58d26a94f9cb3701943702
varsha-mano <52581786+varsha-mano@users.noreply.github.com>
2019-07-06 00:42:13 +0530

    Add files via upload

---
+++ msftplot.py
@@ -0,0 +1,34 @@
+import csv
+import matplotlib.pyplot as plt
+from datetime import datetime as ddt
+import matplotlib.dates as mdates
+
+fileobj = open('msft.csv')
+read = csv.reader(fileobj)
+
+header = next(read)
+
+date = []
+adjclose = []
+
+for row in read:
+    date.append(ddt.strptime(row[0].strip(), '%Y-%m-%d'))
+    adjclose.append(float(row[5]))
+
+fig = plt.figure()
+ax = plt.subplot2grid((1, 1), (0, 0))
+
+for labels in ax.xaxis.get_ticklabels():
+    labels.set_rotation(45)
+
+ax.plot_date(date, adjclose, '-', xdate=True, ydate=False, label='Microsot Data'),
+ax.grid(True)
+ax.xaxis.set_major_formatter(mdates.DateFormatter('%d-%b-%Y'))  # modifying date format
+
+plt.subplots_adjust(left=0.12, bottom=0.2, right=0.9, top=0.95, wspace=0.2, hspace=0.2)
+plt.title('Microsoft Inc')
+plt.xlabel('Date')
+plt.ylabel('Adj.close')
+plt.legend()
+
+plt.show()
